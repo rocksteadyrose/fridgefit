4f8aa2aa5294df1b41445a153d90f2b4706a9f48
Leslie Rose <leslierose@Leslies-MacBook-Pro.local>
2018-04-14 11:09:45 -0500

    adding in map function

--- javascript/app.js
+++ javascript/app.js
@@ -68,16 +68,44 @@ $.ajax({
 	})
 });
 
-function initMap() {
 
-	// Create a map object, and include the MapTypeId to add
-	// to the map type control.
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 55.647, lng: 37.581},
-	  zoom: 11,
-	  
+var map;
+var infowindow;
+
+function initMap() {
+	var pyrmont = {lat: -33.867, lng: 151.195};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: pyrmont,
+	zoom: 15
 	});
 
-	//Associate the styled map with the MapTypeId and set it to display.
-	map.setMapTypeId('styled_map');
-  }
+	infowindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch({
+	location: pyrmont,
+	radius: 500,
+	type: ['store']
+	}, callback);
+}
+
+function callback(results, status) {
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+	for (var i = 0; i < results.length; i++) {
+		createMarker(results[i]);
+	}
+	}
+}
+
+function createMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+	map: map,
+	position: place.geometry.location
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+	infowindow.setContent(place.name);
+	infowindow.open(map, this);
+	});
+}
